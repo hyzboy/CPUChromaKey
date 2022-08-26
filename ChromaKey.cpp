b73d25a7dac156b6b99dd5061ce67e4739996a10
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2022-08-26 21:28:28 +0800

    added output hint.

--- ChromaKey.cpp
+++ ChromaKey.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include"bitmap2d.h"
 
-
-
-int main()
+int main(int argc,char **argv)
 {
-    
+    if(argc<3)
+    {
+        std::cout<<"Example: ChromaKey out.tga in.tga"<<std::endl;
+        return 0;
+    }
+
+    std::cout<<"in:  "<<argv[2]<<std::endl;
+    std::cout<<"out: "<<argv[1]<<std::endl;
+
+
+
+    return 0;
 }
